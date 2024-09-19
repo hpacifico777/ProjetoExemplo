6376d8112982558c69addd167e88898585630802
Henrique <hpacifico19@gmail.com>
2024-09-19 11:24:14 -0300

    Criando tipos de variaveis

--- script.js
+++ script.js
@@ -1,12 +1,51 @@
 // alert("Ola mundo - externo")
 
+//OUTRAS DECLARAÇÕES
+
+//declaração indefinida
+let nome3;
+console.log(nome3)
+
+//declaração vazia
+
+let disciplina=null;
+console.log(disciplina)
+
+//declaração definida
 //DECLARANDO AS VARIÁVEIS
 
 var nome="fiap";
-console.log(nome);
+console.log(nome)
 
 let nome1="cidade";
-console.log(nome1);
+console.log(nome1)
 
 const nome2="javascript";
 console.log(nome2)
+
+//TIPOS DE VARIÁVEIS
+
+//INTEIRO
+
+let idade = 18;
+console.log(idade)
+
+//FLUTUANTES OU COM CASAS DECIMAIS
+
+let valor= 6.40;
+console.group(valor)
+
+//STRING(TEXTO)
+
+let text="fiap"
+console.log(text)
+
+//BOOLEANOS - VERDADEIRO OU FALSO
+
+let aluno=true
+console.log(aluno)
+
+//ARRAY - CONJUNTO DE INFORMAÇÕES
+
+let tecnologias =["JAVA", "C#", "HTML", "CSS"];
+consolge.log(tecnologias)
