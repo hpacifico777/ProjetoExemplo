ea5720cd205a63ec37ebb1ff2924ede3a9d75dbd
Henrique <hpacifico19@gmail.com>
2024-09-19 11:07:39 -0300

    Declarando os tipos de dados var, let, const

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert("Ola mundo - externo")
+// alert("Ola mundo - externo")
+
+//DECLARANDO AS VARIÁVEIS
+
+var nome="fiap";
+console.log(nome);
+
+let nome1="cidade";
+console.log(nome1);
+
+const nome2="javascript";
+console.log(nome2)
